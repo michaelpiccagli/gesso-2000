c40c8883d467ac682bb0062ef665bbb21fbb7818
Michael Piccagli <michaelp_@hotmail.com>
2025-05-23 13:18:44 -0300

    estrutura de pages no app

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import '../styles/header.css';
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="row justify-content-between align-items-center">
+                    <a href="#">
+                        <h1 className="logo no-select">Gesso</h1>
+                    </a>
+                    <nav>
+                        <div className="menu align-items-center" onClick={toggleMenu} aria-label="Menu hamburguer" tabIndex={0} role="button" onKeyPress={toggleMenu}>
+                            <span className={`hamburguer ${menuOpen ? 'open' : ''}`}></span>
+                        </div>
+                        <ul className={menuOpen ? 'open' : ''}>
+                            <li><a href="#Home" onClick={closeMenu}>Home</a></li>
+                            <li><a href="#Projects" onClick={closeMenu}>Projetos</a></li>
+                            <li><a href="#about-us" onClick={closeMenu}>Quem Somos</a></li>
+                            <li><a href="#contact" onClick={closeMenu}>Contato</a></li>
+
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
+
